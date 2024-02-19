d1690211554ce1b245c3aa61eeaf0e014daa9a0b
mixvert <bajkowski1e@gmail.com>
2024-02-19 23:53:44 +0100

    Bug fix with failure to send email from confirmation email address

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,16 +2,93 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
+
   try {
     await newUser.save();
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const confirmationLink = `${process.env.APP_URL}/auth/confirm-email/${token}`;
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: email,
+      subject: "Confirmation of registration",
+      html: `Click <a href="${confirmationLink}">here</a> ,to confirm your registration.`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          success: false,
+          message: "Error while sending confirmation email.",
+        });
+      }
+      console.log("Email sent: " + info.response);
+    });
+
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
+  }
+};
+
+export const confirmEmail = async (req, res, next) => {
+  const { token } = req.params;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found." });
+    }
+
+    if (user.emailConfirmed) {
+      return res.status(400).json({
+        success: false,
+        message: "Email address has already been confirmed.",
+      });
+    }
+
+    user.emailConfirmed = true;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Email address confirmed successfully.",
+    });
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ success: false, message: "The token has expired." });
+    }
+
+    return res.status(500).json({
+      success: false,
+      message: "Error while confirming email address.",
+    });
   }
 };
 
@@ -19,13 +96,21 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
+
     if (!user) {
       return next(errorHandler(404, "User not found"));
     }
+
+    if (!user.emailConfirmed) {
+      return next(errorHandler(400, "Email not confirmed"));
+    }
+
     const isPasswordCorrect = bcryptjs.compareSync(password, user.password);
+
     if (!isPasswordCorrect) {
       return next(errorHandler(400, "Invalid credentials"));
     }
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     const { password: userPassword, ...userData } = user._doc;
     const expiryDate = new Date(Date.now() + 3600000);
@@ -85,5 +170,5 @@ export const google = async (req, res, next) => {
 };
 
 export const signout = async (req, res, next) => {
-  res.clearCookie("token").status(200).json({ message: "Logged out" });
+  res.clearCookie("access_token").status(200).json({ message: "Logged out" });
 };
