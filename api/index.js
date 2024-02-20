769592c07b4b6a7a7973decc08ed0a39bffabc99
mixvert <bajkowski1e@gmail.com>
2024-02-20 08:03:51 +0100

    Server fix error

--- api/index.js
+++ api/index.js
@@ -17,15 +17,14 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
 
-const publicPath = path.join(__dirname, 'client', 'dist');
-app.use(express.static(publicPath));
+const __dirname = path.resolve();
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(path.join(__dirname, "/client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.use(express.json());
