a9b1e594ed752c8c38b9730bb10ef020dc5590f0
mixvert <bajkowski1e@gmail.com>
2024-02-26 16:34:10 +0100

    Server minor changes

--- api/index.js
+++ api/index.js
@@ -22,10 +22,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.use(cors());
