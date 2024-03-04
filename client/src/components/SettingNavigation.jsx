ad82e8811c501c5fb068341b24275d645516a13d
mixvert <bajkowski1e@gmail.com>
2024-03-04 13:13:26 +0100

    Create profile navigation

---
+++ client/src/components/SettingNavigation.jsx
@@ -0,0 +1,42 @@
+import {Link, useLocation} from "react-router-dom";
+
+export default function SettingNavigation () {
+    const location = useLocation();
+    const isProfileDetailsActive = location.pathname === '/profile';
+    const isAccountSettingsActive = location.pathname === '/account-settings';
+
+    return (
+        <div className="col-span-4">
+            <div className="text-xl px-4 pb-4">
+                Settings
+            </div>
+            <div className={`bg-slate-200 bg-opacity-0 font-lg transition duration-300 ease-in-out hover:shadow-md 
+            hover:shadow-slate-200/50 hover:transition hover:duration-300 hover:ease-in-out hover:bg-opacity-40`}>
+                <Link
+                    to="/profile"
+                    className={`text-black ${isProfileDetailsActive ? 'text-opacity-100' : 'text-opacity-50'}`}
+                >
+                    <div className={`mb-2 px-4 py-3`}>
+                        Profile details
+                    </div>
+                </Link>
+            </div>
+            <div className={`bg-slate-200 bg-opacity-0  font-lg transition duration-300 ease-in-out hover:shadow-md 
+            hover:shadow-slate-200/50 hover:transition hover:duration-300 hover:ease-in-out hover:bg-opacity-40`}>
+                <Link
+                    to="/account-settings"
+                    className={`text-black ${isAccountSettingsActive ? 'text-opacity-100' : 'text-opacity-50'}`}
+                >
+                    <div className={`mb-2 px-4 py-3`}>
+                        Account settings
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
