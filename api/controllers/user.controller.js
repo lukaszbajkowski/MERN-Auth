c64130ad33fbc217e4bccf0f6633463d4017dec1
mixvert <bajkowski1e@gmail.com>
2024-03-06 19:01:13 +0100

    Adding updateUserProfilePicture And optimizing the structure

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,56 +1,62 @@
 import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js";
+import {errorHandler} from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
-export const test = (req, res) => {
-  res.json({
-    message: "API is working!",
-  });
+const updateUserFields = async (req, res, next, updateFields) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return next(errorHandler(404, "User not found"));
+        }
+
+        if (user.googleAccount) {
+            return next(errorHandler(403, "Cannot update profile for Google account."));
+        }
+
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {$set: updateFields},
+            {new: true}
+        );
+
+        const {password, ...rest} = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can update only your account!"));
-  }
-  
-  try {
-    const user = await User.findById(req.params.id);
+    const updateFields = {
+        ...(req.body.username && {username: req.body.username}),
+        ...(req.body.email && {email: req.body.email}),
+        ...(req.body.password && {password: req.body.password}),
+    };
 
-    if (user.googleAccount) {
-      return next(errorHandler(403, "Cannot update profile for Google account."));
-    }
+    await updateUserFields(req, res, next, updateFields);
+};
 
-    if (req.body.password) {
-      req.body.password = bcryptjs.hashSync(req.body.password, 10);
-    }
+export const updateUserProfilePicture = async (req, res, next) => {
+    const updateFields = {
+        profilePicture: req.body.profilePicture,
+    };
 
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: {
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          profilePicture: req.body.profilePicture,
-        },
-      },
-      { new: true }
-    );
-    const { password, ...rest } = updatedUser._doc;
-    res.status(200).json(rest);
-  } catch (error) {
-    next(error);
-  }
+    await updateUserFields(req, res, next, updateFields);
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can delete only your account!"));
-  }
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("User has been deleted...");
-  } catch (error) {
-    next(error);
-  }
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can delete only your account!"));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch (error) {
+        next(error);
+    }
 };
