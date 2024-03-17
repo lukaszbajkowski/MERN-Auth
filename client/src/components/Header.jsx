e4471c9d6aeafde0f3166626690b2107c56ae5b2
mixvert <bajkowski1e@gmail.com>
2024-03-17 22:44:16 +0100

    Implementation of dropdown design to heder

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,40 @@
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
+import Example from "./HeaderDropdown";
 
-export default function Header() {
-  const { currentUser } = useSelector((state) => state.user);
+export default function Header () {
+    const {currentUser} = useSelector((state) => state.user);
 
-  return (
-    <div className="bg-slate-200">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to="/" className="my-auto">
-          <h1 className="font-bold text-3xl">Auth App</h1>
-        </Link>
-        <ul className="flex gap-4">
-          <Link to="/" className="my-auto">
-            <li>Home</li>
-          </Link>
-          <Link to="about" className="my-auto">
-            <li>About</li>
-          </Link>
-          <Link to={currentUser ? "/profile" : "/sign-in"} className="my-auto">
-            {currentUser ? (
-              <img
-                src={currentUser.profilePicture}
-                alt="profilePicture"
-                className="h-8 w-8 rounded-full object-cover"
-              />
-            ) : (
-              <li>Sign In</li>
-            )}
-          </Link>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-slate-200">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link to="/" className="my-auto">
+                    <h1 className="font-bold text-3xl">Auth App</h1>
+                </Link>
+                <ul className="flex gap-4">
+                    <Link to="/" className="my-auto">
+                        <li>Home</li>
+                    </Link>
+                    <Link to="about" className="my-auto">
+                        <li>About</li>
+                    </Link>
+                    {/*<Link to={currentUser ? "/profile" : "/sign-in"} className="my-auto">*/}
+                    {/*    {currentUser ? (*/}
+                    {/*        <img*/}
+                    {/*            src={currentUser.profilePicture}*/}
+                    {/*            alt="profilePicture"*/}
+                    {/*            className="h-8 w-8 rounded-full object-cover"*/}
+                    {/*        />*/}
+                    {/*    ) : (*/}
+                    {/*        <li>Sign In</li>*/}
+                    {/*    )}*/}
+                    {/*</Link>*/}
+                    <Example
+                      currentUser={currentUser}
+                  />
+                </ul>
+            </div>
+
+        </div>
+    );
 }
---
+++ client/src/components/HeaderDropdown.jsx
@@ -0,0 +1,122 @@
+import {Fragment} from 'react'
+import {Menu, Transition} from '@headlessui/react'
+
+function classNames (...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Example ({currentUser}) {
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+            <div>
+                <Menu.Button className="inline-flex w-full justify-center py-1">
+                    <img
+                        src={currentUser.profilePicture}
+                        alt="profilePicture"
+                        className="h-8 w-8 py-auto rounded-full object-cover"
+                    />
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items
+                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Profile details
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Account settings
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Security settings
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Personalization
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Share
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Logout
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
+}
