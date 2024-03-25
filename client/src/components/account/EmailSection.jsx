f0a2882d9338431b9f26d0ef2d9cf5c91003c006
mixvert <bajkowski1e@gmail.com>
2024-03-25 14:13:00 +0100

    Fix style errors

--- client/src/components/account/EmailSection.jsx
+++ client/src/components/account/EmailSection.jsx
@@ -2,39 +2,43 @@ import PropTypes from 'prop-types';
 import {CheckIcon, XMarkIcon} from '@heroicons/react/20/solid';
 
 const EmailSection = ({currentUser}) => {
+    const emailConfirmed = currentUser.emailConfirmed;
+    const isGoogleAccount = currentUser.googleAccount;
+
+    const loginColClasses = isGoogleAccount ? "col-end-7" : "md:col-end-5 lg:col-end-6";
+    const userColClasses = isGoogleAccount ? "col-start-7 lg:col-start-7 col-end-13" : "md:col-start-5 lg:col-start-6 col-end-10";
+
+    const emailConfirmedText = isGoogleAccount ? 'Email confirmed via Google Account' : (emailConfirmed ? 'Email confirmed' : 'Email not confirmed');
+    const emailConfirmedColor = isGoogleAccount ? 'text-green-700' : (emailConfirmed ? 'text-green-700' : 'text-red-700');
+    const emailConfirmedIcon =
+        isGoogleAccount ?
+            <CheckIcon className="h-5 w-5 my-auto me-1 text-green-700" aria-hidden="true"/>
+            : (emailConfirmed ?
+                    <CheckIcon className="h-5 w-5 my-auto me-1 text-green-700" aria-hidden="true"/>
+                    :
+                    <XMarkIcon className="h-5 w-5 my-auto me-1 text-red-700" aria-hidden="true"/>
+            )
+    ;
+
     return (
         <>
-            <div
-                className={`col-start-1 md:col-end-${currentUser.googleAccount ? '7' : '5'} lg:col-end-${currentUser.googleAccount ? '7' : '6'} text-black text-start my-auto`}>
+            <div className={`col-start-1 ${loginColClasses} text-black text-start my-auto`}>
                 <h1>Email</h1>
             </div>
-            <div
-                className={`col-start-1 md:col-start-${currentUser.googleAccount ? '7' : '5'} lg:col-start-${currentUser.googleAccount ? '7' : '6'} col-end-${currentUser.googleAccount ? '13' : '10'} flex flex-col my-auto`}>
+            <div className={`col-start-1 ${userColClasses} flex flex-col my-auto`}>
                 <div className="grid grid-cols-12">
                     <div className="col-start-1 col-end-13 break-all">
                         {currentUser.email}
                     </div>
-                    <div className="col-start-1 col-end-13">
-                        {currentUser.googleAccount ? (
-                            <span className="flex text-green-700">
-                                <CheckIcon className="h-5 w-5 my-auto me-1 text-green-700" aria-hidden="true"/>
-                                Email confirmed via Google Account
-                            </span>
-                        ) : currentUser.emailConfirmed ? (
-                            <span className="flex text-green-700">
-                                <CheckIcon className="h-5 w-5 my-auto me-1 text-green-700" aria-hidden="true"/>
-                                Email confirmed
-                            </span>
-                        ) : (
-                            <span className="flex text-red-700">
-                                <XMarkIcon className="h-5 w-5 my-auto me-1 text-red-700" aria-hidden="true"/>
-                                Email not confirmed
-                            </span>
-                        )}
+                    <div className={`col-start-1 col-end-13 ${emailConfirmedColor}`}>
+                        <span className="flex">
+                            {emailConfirmedIcon}
+                            {emailConfirmedText}
+                        </span>
                     </div>
                 </div>
             </div>
-            {!currentUser.googleAccount && (
+            {!isGoogleAccount && (
                 <div className="col-start-10 col-end-13 my-auto flex justify-end">
                     <button
                         className="bg-slate-700 text-white rounded-lg py-3 px-6 capitalize shadow-md shadow-slate-700-50 transition duration-300 ease-in-out hover:transition hover:duration-300 hover:ease-in-out hover:opacity-95 disabled:opacity-80 inline-block"
@@ -52,7 +56,11 @@ const EmailSection = ({currentUser}) => {
 };
 
 EmailSection.propTypes = {
-    currentUser: PropTypes.object.isRequired,
+    currentUser: PropTypes.shape({
+        googleAccount: PropTypes.bool.isRequired,
+        emailConfirmed: PropTypes.bool.isRequired,
+        email: PropTypes.string.isRequired
+    }).isRequired,
 };
 
 export default EmailSection;
--- client/src/components/account/LoginSection.jsx
+++ client/src/components/account/LoginSection.jsx
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types';
 
 const LoginSection = ({currentUser}) => {
+    const isGoogleAccount = currentUser.googleAccount;
+    const loginColClasses = isGoogleAccount ? "col-end-7" : "md:col-end-5 lg:col-end-6";
+    const userColClasses = isGoogleAccount ? "col-start-7 lg:col-start-7 col-end-13" : "md:col-start-5 lg:col-start-6 col-end-10";
+
     return (
         <>
-            <div
-                className={`col-start-1 md:col-end-${currentUser.googleAccount ? '7' : '5'} lg:col-end-${currentUser.googleAccount ? '7' : '6'} text-black text-start my-auto`}>
+            <div className={`col-start-1 ${loginColClasses} text-black text-start my-auto`}>
                 <h1>Login</h1>
             </div>
-            <div
-                className={`col-start-1 md:col-start-${currentUser.googleAccount ? '7' : '5'} lg:col-start-${currentUser.googleAccount ? '7' : '6'} col-end-${currentUser.googleAccount ? '13' : '10'} flex flex-col my-auto`}>
+            <div className={`col-start-1 ${userColClasses} flex flex-col my-auto`}>
                 <div className="grid grid-cols-12">
                     <div className="col-start-1 col-end-13 break-all">
                         {currentUser.username}
                     </div>
                 </div>
             </div>
-            {!currentUser.googleAccount && (
+            {!isGoogleAccount && (
                 <div className="col-start-10 col-end-13 my-auto flex justify-end">
                     <button
                         className="bg-slate-700 text-white rounded-lg py-3 px-6 capitalize shadow-md shadow-slate-700-50 transition duration-300 ease-in-out hover:transition hover:duration-300 hover:ease-in-out hover:opacity-95 disabled:opacity-80 inline-block"
@@ -33,7 +35,10 @@ const LoginSection = ({currentUser}) => {
 };
 
 LoginSection.propTypes = {
-    currentUser: PropTypes.object.isRequired,
+    currentUser: PropTypes.shape({
+        googleAccount: PropTypes.bool.isRequired,
+        username: PropTypes.string.isRequired
+    }).isRequired,
 };
 
 export default LoginSection;
